cef103f7acc74e33fa58c0214a997edfe61de040
Taiki Yoshino <s21C1137QR@s.chibakoudai.jp>
2023-01-01 20:54:23 +0900

    rev

--- build/mypkg/build/lib/mypkg/talker.py
+++ build/mypkg/build/lib/mypkg/talker.py
@@ -1,16 +1,34 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2022 Taiki Yoshino
+# SPDX-License-Identifier: BSD-3-Clause
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query #使う型を変更
- 
-def cb(request, response):
-    if request.name == "吉野泰生":
-        response.age = 19
-    else:
-        response.age = 255
+from std_msgs.msg import Int16
 
-    return response
+class Talker():
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb)
 
+    def cb(self):      #インデントをあげてselfを引数に
+        msg = Int16()
+        msg.data = self.n     #talker -> self
+        self.pub.publish(msg) #talker -> self
+        self.n += 1
+        
 rclpy.init()
 node = Node("talker")
-srv = node.create_service(Query, "query", cb) #サービスの作成
+talker = Talker(node)
 rclpy.spin(node)
+
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
+
+
